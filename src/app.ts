3970fca3158e622e3aa4f85c716e9fe1139ebef4
rey <rhernandez@orfis.gob.mx>
2025-02-05 16:14:52 -0600

    it works on 2025

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,44 @@
-import "./bootstrap"
-import "express-async-errors"
-import express, {Request, Response, NextFunction} from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import bodyParser from "body-parser"
+/// <reference path="./@types/express.d.ts" />
+import express from "express";
 
-import "./database"
-import AppError from "./errors/AppError"
-import routes from './routes'
-import path from "path"
+import "./bootstrap";
+import "express-async-errors";
+import { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
+import "./database";
+import AppError from "./errors/AppError";
+import routes from "./routes";
+import path from "path";
 
-const app = express()
+const app = express();
 
 app.use(
-    cors({
-        credentials: true,
-        origin: process.env.FRONTEND_URL
-    })
-)
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+);
 
-const publicFolder = path.resolve(__dirname, "..", "public")
+const publicFolder = path.resolve(__dirname, "..", "public");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static(publicFolder));
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use("/public", express.static(publicFolder))
+app.get("/", (req, res) => {
+  res.json({ messsage: "Opuz" });
+});
 
-app.get('/',(req,res)=>{ res.json({messsage:'Opuz'}) })
-
-app.use(routes)
+app.use(routes);
 
 app.use(async (err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ error: err.message });
+  }
 
-    if(err instanceof AppError){
-        return res.status(err.statusCode).json({error: err.message})
-    }
+  return res.status(500).json({ error: "Internal server error" });
+});
 
-    return res.status(500).json({error: "Internal server error"})
-}) 
-
-
-
-export default app
+export default app;
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import {
+ import {
     Table,
     Column,
     CreatedAt,
